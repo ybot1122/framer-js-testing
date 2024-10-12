08e5988f993c4e2777cd5fe470fdace78aee6762
Toby Liu <ybot1122@gmail.com>
2024-10-11 18:18:26 -0700

    wheel listener

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,43 @@
 import './App.css'
-import { useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import {
   motion,
+  useMotionValueEvent,
   useScroll,
   useSpring,
-  useTransform,
-  MotionValue
 } from "framer-motion";
 
-function useParallax(value: MotionValue<number>, distance: number) {
-  return useTransform(value, [0, 1], [-distance, distance]);
+enum SlideState {
+  Past = 'past',
+  Current = 'current',
+  Future = 'future',
 }
 
-function Image({ id }: { id: number }) {
-  const ref = useRef(null);
-  const { scrollYProgress } = useScroll({ target: ref });
-  const y = useParallax(scrollYProgress, 300);
+function Section({ index, total, state }: { index: number, total: number, state: SlideState }) {
+  const { scrollYProgress } = useScroll();
+  const myStart = (1 / total) * index;
+  const myEnd = myStart + (1 / total);
+
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+
+    if (latest >= myStart && latest < myEnd) {
+      console.log("Container " + index + " in view")
+    }
+  })
+
+
+  let className = '';
+  if (state === SlideState.Past) {
+    className = 'past'
+  } else if (state === SlideState.Future) {
+    className = 'future'
+  }
 
   return (
-    <section>
-      <div ref={ref}>
-        <img src={`/${id}.jpg`} alt="A London skyscraper" />
-      </div>
-      <motion.h2 style={{ y }}>{`#00${id}`}</motion.h2>
+    <section 
+    className={className}
+    style={{background: index % 2 === 0 ? 'blue' : 'red', zIndex: total - index}}>
+      <motion.h2>{`#00${index}`}</motion.h2>
     </section>
   );
 }
@@ -35,12 +50,45 @@ export default function App() {
     restDelta: 0.001
   });
 
+  const activeIndRef = useRef(0);
+  const [activeInd, setActiveInd] = useState(activeIndRef.current);
+
+  const wheelCb = useCallback((event: WheelEvent) => {
+      console.log(event.deltaY, activeInd, activeIndRef.current)
+
+      if (event.deltaY < 0 && activeIndRef.current > 0) {
+        activeIndRef.current -= 1;
+        setActiveInd(activeIndRef.current)
+      } else if (event.deltaY > 0 && activeIndRef.current < 5) {
+        activeIndRef.current += 1;
+        setActiveInd(activeIndRef.current)
+      }
+
+      console.log(activeInd, activeIndRef.current)
+
+  }, [setActiveInd])
+
+  useEffect(() => {
+    addEventListener("wheel", wheelCb);
+    return () => removeEventListener('wheel', wheelCb)
+  }, [wheelCb])
+  
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    console.log("Page scroll: ", latest)
+  })
+
   return (
     <>
-      {[1, 2, 3, 4, 5].map((image) => (
-        <Image id={image} />
-      ))}
-      <motion.div className="progress" style={{ scaleX }} />
+      {[0, 1, 2, 3, 4, 5].map((ind) => {
+        let state = SlideState.Current;
+        if (ind < activeInd) {
+          state = SlideState.Past
+        } else if (ind > activeInd) {
+          state = SlideState.Future
+        }
+          return <Section index={ind} total={6} key={ind} state={state} />
+      }
+      )}
     </>
   );
 }
