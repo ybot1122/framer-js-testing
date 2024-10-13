f1eb0104ecbadaded34a9f3ea1a6b1c953d7927b
Toby Liu <ybot1122@gmail.com>
2024-10-12 19:51:38 -0700

    pinter and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import "./App.css";
 import { useCallback, useEffect, useRef, useState } from "react";
 
 import { Lethargy } from "lethargy-ts";
@@ -8,107 +8,164 @@ const SWIPE_THRESHOLD_MS = 200;
 
 const lethargy = new Lethargy({
   delay: 50,
-  sensitivity: 40
+  sensitivity: 40,
 });
 
-function Section({ index, total, activeInd }: { index: number, total: number, activeInd: number }) {
-  let top = '0';
+function Section({
+  index,
+  total,
+  activeInd,
+  yOffset,
+}: {
+  index: number;
+  total: number;
+  activeInd: number;
+  yOffset: number;
+}) {
+  let top = 0 + "px";
   if (index < activeInd) {
-    top = ((activeInd - index) * -100) + 'vh';
+    top = (activeInd - index) * -100 + 0 + "vh";
   } else if (index > activeInd) {
-    top = ((index - activeInd) * 100) + 'vh';
+    top = (index - activeInd) * 100 - 0 + "vh";
   }
 
   return (
-    <section 
-    style={{background: index % 2 === 0 ? 'blue' : 'red', zIndex: total - index, top}}>
+    <section
+      style={{
+        background: index % 2 === 0 ? "blue" : "red",
+        zIndex: total - index,
+        top,
+        marginTop: yOffset,
+      }}
+    >
       <h2>{`#00${index}`}</h2>
     </section>
   );
 }
 
 export default function App() {
-  const pointerStartData = useRef<undefined | {timestamp: number, y: number}>();
+  const pointerStartData = useRef<
+    undefined | { timestamp: number; y: number }
+  >();
   const activeIndRef = useRef(0);
   const throttledRef = useRef(false);
   const [activeInd, setActiveInd] = useState(activeIndRef.current);
+  const [yOffset, setYOffset] = useState(0);
 
   const goNext = useCallback(() => {
     if (activeIndRef.current === 5) return;
     if (throttledRef.current) {
-      console.log('throttled');
+      console.log("throttled");
       return;
     }
 
     throttledRef.current = true;
     activeIndRef.current += 1;
     setActiveInd(activeIndRef.current);
-    setTimeout(() => throttledRef.current = false, SLIDE_THROTTLE_MS);
-  }, [setActiveInd])
+    setTimeout(() => (throttledRef.current = false), SLIDE_THROTTLE_MS);
+  }, [setActiveInd]);
 
   const goPrevious = useCallback(() => {
     if (activeIndRef.current === 0) return;
     if (throttledRef.current) {
-      console.log('throttled');
+      console.log("throttled");
       return;
     }
 
     throttledRef.current = true;
     activeIndRef.current -= 1;
     setActiveInd(activeIndRef.current);
-    setTimeout(() => throttledRef.current = false, SLIDE_THROTTLE_MS);
-  }, [setActiveInd])
+    setTimeout(() => (throttledRef.current = false), SLIDE_THROTTLE_MS);
+  }, [setActiveInd]);
 
-
-  const wheelCb = useCallback((event: WheelEvent) => {
-    const isIntentional = lethargy.check(event);
-    if (isIntentional) {
+  const wheelCb = useCallback(
+    (event: WheelEvent) => {
+      const isIntentional = lethargy.check(event);
+      if (isIntentional) {
         if (event.deltaY < 0 && activeIndRef.current > 0) {
           goPrevious();
         } else if (event.deltaY > 0 && activeIndRef.current < 5) {
           goNext();
-        }        
-    }
-  }, [goNext, goPrevious])
+        }
+      }
+    },
+    [goNext, goPrevious],
+  );
 
   const pointerDownCb = useCallback((event: PointerEvent) => {
+    console.log("pdown");
+
     pointerStartData.current = {
       timestamp: Date.now(),
-      y: event.clientY
-    }
-  }, [])
+      y: event.clientY,
+    };
+  }, []);
 
-  const pointerUpCb = useCallback((event: PointerEvent) => {
-
-    if (!pointerStartData.current) return;
-    if (Date.now() - pointerStartData.current?.timestamp > SWIPE_THRESHOLD_MS) return;
-    if (event.y < pointerStartData.current.y) {
-      goNext();
-    } else {
-      goPrevious();
-    }
-
+  const pointerCancelCb = useCallback(() => {
+    console.log("pcancel");
+    setYOffset(0);
     pointerStartData.current = undefined;
-  }, [goNext, goPrevious])
+  }, [setYOffset]);
 
+  const pointerUpCb = useCallback(
+    (event: PointerEvent) => {
+      console.log("pup");
+
+      setYOffset(0);
+
+      if (!pointerStartData.current) return;
+      if (
+        Date.now() - pointerStartData.current?.timestamp <
+        SWIPE_THRESHOLD_MS
+      ) {
+        if (event.y < pointerStartData.current.y) {
+          goNext();
+        } else {
+          goPrevious();
+        }
+      }
+      pointerStartData.current = undefined;
+    },
+    [goNext, goPrevious, setYOffset],
+  );
+
+  const pointerMoveCb = useCallback(
+    (event: PointerEvent) => {
+      if (!pointerStartData.current) return;
+
+      setYOffset(event.clientY - pointerStartData.current.y);
+    },
+    [setYOffset],
+  );
 
   useEffect(() => {
     addEventListener("wheel", wheelCb);
     addEventListener("pointerdown", pointerDownCb);
     addEventListener("pointerup", pointerUpCb);
+    addEventListener("pointermove", pointerMoveCb);
+    addEventListener("pointercancel", pointerCancelCb);
     return () => {
-      removeEventListener('wheel', wheelCb)
-      removeEventListener("pointerdown", pointerDownCb)
-      removeEventListener("pointerup", pointerUpCb)
-    }
-  }, [wheelCb, pointerDownCb, pointerUpCb])
-  
+      removeEventListener("wheel", wheelCb);
+      removeEventListener("pointerdown", pointerDownCb);
+      removeEventListener("pointerup", pointerUpCb);
+      removeEventListener("pointermove", pointerMoveCb);
+      removeEventListener("pointercancel", pointerCancelCb);
+    };
+  }, [wheelCb, pointerDownCb, pointerUpCb]);
+
   return (
     <>
       {[0, 1, 2, 3, 4, 5].map((ind) => {
-          return <Section index={ind} total={6} key={ind} activeInd={activeInd} />
-      }
-      )}
+        return (
+          <Section
+            index={ind}
+            total={6}
+            key={ind}
+            activeInd={activeInd}
+            yOffset={yOffset}
+          />
+        );
+      })}
     </>
   );
 }
