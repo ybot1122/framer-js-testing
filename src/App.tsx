71a30b1aee384c8cf24e62d05d42cc5d5ef59261
Toby Liu <ybot1122@gmail.com>
2024-10-21 19:08:02 -0700

    bad interface

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,18 @@ export default function App() {
   return (
     <main>
       <ReactFullpageSlideshow items={[
-        <Section label="slide1" />,
-        <Section label="slide2" />,
-        <Section label="slide3" />
-      ]} />
+        (goToSlide) => <Section label="slide1" goToSlide={goToSlide} />,
+        (goToSlide) => <Section label="slide2" goToSlide={goToSlide} />,
+        (goToSlide) => <Section label="slide3" goToSlide={goToSlide} />,
+        (goToSlide) => <Section label="slide4" goToSlide={goToSlide} />,
+
+        (goToSlide) => <Section label="slide5" goToSlide={goToSlide} />,
+]} />
     </main>
   );
 }
 
-const Section = ({label}: {label: string}) => {
+const Section = ({label, goToSlide}: {label: string, goToSlide: any}) => {
 
-  return <div>{label}</div>
+  return <div>{label}<button onClick={() => goToSlide(2)}>go to slide 2</button></div>
 }
