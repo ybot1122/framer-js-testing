e4c0ffe37232ad037ae2c0557258ffd49c28594b
Toby Liu <ybot1122@gmail.com>
2024-10-21 19:44:45 -0700

    hi

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import { ReactFullpageSlideshow } from "@ybot1122/react-fullpage-slideshow";
 import "./App.css";
 
 export default function App() {
   return (
     <main>
-      <ReactFullpageSlideshow items={[
-        (goToSlide) => <Section label="slide1" goToSlide={goToSlide} />,
-        (goToSlide) => <Section label="slide2" goToSlide={goToSlide} />,
-        (goToSlide) => <Section label="slide3" goToSlide={goToSlide} />,
-        (goToSlide) => <Section label="slide4" goToSlide={goToSlide} />,
-
-        (goToSlide) => <Section label="slide5" goToSlide={goToSlide} />,
-]} />
+      <div style={{
+        height: '100vh',
+        width: '100vw,'
+      }}>Hi</div>
+      <div style={{
+        height: '100vh',
+        width: '100vw,'
+      }}>Hi again</div>
+      <div style={{
+        height: '100vh',
+        width: '100vw,'
+      }}>Hi hi</div>
     </main>
   );
-}
-
-const Section = ({label, goToSlide}: {label: string, goToSlide: any}) => {
-
-  return <div>{label}<button onClick={() => goToSlide(2)}>go to slide 2</button></div>
 }
