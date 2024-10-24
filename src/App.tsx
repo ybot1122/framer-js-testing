65005c047ff1670df2e2dc40e013b3958032bfe0
Toby Liu <ybot1122@gmail.com>
2024-10-23 18:56:14 -0700

    prev next

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,41 @@
 import "./App.css";
+import { ReactFullpageSlideshow } from "@ybot1122/react-fullpage-slideshow";
 
 export default function App() {
   return (
     <main>
-      <div style={{
-        height: '100vh',
-        width: '100vw,'
-      }}>Hi contain</div>
-      <div style={{
-        height: '100vh',
-        width: '100vw,'
-      }}>Hi again</div>
-      <div style={{
-        height: '100vh',
-        width: '100vw,'
-      }}>Hi hi</div>
+
+<ReactFullpageSlideshow
+
+
+    items={[
+      ({goToSlide, goToNextSlide}) => <Slide goToSlide={goToSlide} goToNextSlide={goToNextSlide} label="slide one" />,
+      ({goToSlide, goToNextSlide}) => <Slide goToSlide={goToSlide} goToNextSlide={goToNextSlide} label="slide two" />,
+      ({goToSlide, goToNextSlide}) => <Slide goToSlide={goToSlide} goToNextSlide={goToNextSlide} label="slide three" />,
+      ({goToSlide, goToNextSlide}) => <Slide goToSlide={goToSlide} goToNextSlide={goToNextSlide} label="slide four" />,
+      (api) => <Slide {...api} label="slide five" />,
+    ]}
+></ReactFullpageSlideshow>
+
     </main>
   );
+}
+
+const Slide = ({goToSlide, goToNextSlide, label, goToPreviousSlide}: any) => {
+
+  return (
+
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <h1>{label}</h1>
+      <button onClick={() => goToSlide(0)}>Go to Slide 0</button>
+      <button onClick={() => goToNextSlide()}>Next Slide</button>
+      <button onClick={() => goToPreviousSlide()}>Prev Slide</button>
+
+    </div>
+
+  )
 }
